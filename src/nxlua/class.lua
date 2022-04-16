d47caeaee55645577ea734e753121f478ebab1e5
niu2x <niu2x@icloud.com>
2022-04-16 19:49:44 +0800

    add class.lua

---
+++ src/nxlua/class.lua
@@ -0,0 +1,23 @@
+local baseClass = {class = true}
+baseClass.__index = baseClass
+
+function baseClass:ctor()
+end
+
+function baseClass:new(...)
+	local classObj = self
+	local instance = {}
+	setmetatable(instance, classObj)
+	instance:ctor(...)
+	return instance
+end
+
+local function class(super)
+	super = super or baseClass
+	local classObj = {class = true}
+	classObj.__index = classObj
+	setmetatable(classObj, super)
+	return classObj
+end
+
+return class
