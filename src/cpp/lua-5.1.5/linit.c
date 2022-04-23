dce0b8bf518d9e43e92993bb6f47cd7dcb4f2d6f
niu2x <niu2x@icloud.com>
2022-04-23 18:31:31 +0800

    add lua-5.1.5

--- src/cpp/lua-5.1.5/linit.c
+++ src/cpp/lua-5.1.5/linit.c
@@ -4,25 +4,30 @@
 ** See Copyright Notice in lua.h
 */
 
+
 #define linit_c
 #define LUA_LIB
 
 #include "lua.h"
 
-#include "lauxlib.h"
 #include "lualib.h"
+#include "lauxlib.h"
 
-static const luaL_Reg lualibs[] = {{"", luaopen_base},
-                                   {LUA_LOADLIBNAME, luaopen_package},
-                                   {LUA_TABLIBNAME, luaopen_table},
-                                   {LUA_IOLIBNAME, luaopen_io},
-                                   {LUA_OSLIBNAME, luaopen_os},
-                                   {LUA_STRLIBNAME, luaopen_string},
-                                   {LUA_MATHLIBNAME, luaopen_math},
-                                   {LUA_DBLIBNAME, luaopen_debug},
-                                   {NULL, NULL}};
 
-LUALIB_API void luaL_openlibs(lua_State *L) {
+static const luaL_Reg lualibs[] = {
+  {"", luaopen_base},
+  {LUA_LOADLIBNAME, luaopen_package},
+  {LUA_TABLIBNAME, luaopen_table},
+  {LUA_IOLIBNAME, luaopen_io},
+  {LUA_OSLIBNAME, luaopen_os},
+  {LUA_STRLIBNAME, luaopen_string},
+  {LUA_MATHLIBNAME, luaopen_math},
+  {LUA_DBLIBNAME, luaopen_debug},
+  {NULL, NULL}
+};
+
+
+LUALIB_API void luaL_openlibs (lua_State *L) {
   const luaL_Reg *lib = lualibs;
   for (; lib->func; lib++) {
     lua_pushcfunction(L, lib->func);
@@ -30,3 +35,4 @@ LUALIB_API void luaL_openlibs(lua_State *L) {
     lua_call(L, 1, 0);
   }
 }
+
---
+++ src/cpp/nxlua/main.cpp
@@ -0,0 +1,10 @@
+extern "C" {
+#include "lua.h"
+#include "luaxlib.h"
+}
+
+int main()
+{
+    auto L = luaL_newstate();
+    return 0;
+}
