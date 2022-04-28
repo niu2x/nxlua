eaa94bdca4293c6aceb4821b2413915c896ab878
niu2x <niu2x@icloud.com>
2022-04-28 13:30:14 +0800

    add lua_pipe

---
+++ src/cpp/nxlua/lua_pipe.cpp
@@ -0,0 +1,15 @@
+#include "lua_pipe.h"
+
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+
+#include "nxlua/pipe.h"
+
+namespace nxlua {
+
+void lua_pipe_open(lua_State* L) { }
+
+} // namespace nxlua
---
+++ src/cpp/nxlua/lua_pipe.h
@@ -0,0 +1,12 @@
+#ifndef NXLUA_LUA_PIPE_H
+#define NXLUA_LUA_PIPE_H
+
+struct lua_State;
+
+namespace nxlua {
+
+extern void lua_pipe_open(lua_State* L);
+
+};
+
+#endif
