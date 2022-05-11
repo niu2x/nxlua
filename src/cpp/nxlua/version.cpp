71b3e0a7c0ccbed4159e64bca2dfbf61ad877f32
niu2x <niu2x@icloud.com>
2022-05-12 00:43:25 +0800

    add version info to _G._VERSION

---
+++ src/cpp/nxlua/version.cpp
@@ -0,0 +1,35 @@
+#include "os_ext.h"
+
+#include <filesystem>
+
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+}
+
+#include "utils.h"
+
+#define NAME _VERSION
+
+namespace nxlua {
+
+
+void lua_version_open(lua_State* L)
+{
+    lua_newtable(L);
+
+    lua_pushnumber(L, NXLUA_PROJECT_VERSION_MAJOR);
+    lua_setfield(L, -2, "major");
+
+    lua_pushnumber(L, NXLUA_PROJECT_VERSION_MINOR);
+    lua_setfield(L, -2, "minor");
+
+    lua_pushnumber(L, NXLUA_PROJECT_VERSION_PATCH);
+    lua_setfield(L, -2, "patch");
+
+    lua_setglobal(L, "_VERSION");
+}
+
+
+} // namespace nxlua
--- src/cpp/nxlua/version.h
+++ src/cpp/nxlua/version.h
@@ -1,4 +1,13 @@
 #ifndef NXLUA_VERSION_H
 #define NXLUA_VERSION_H
 
+struct lua_State;
+
+namespace nxlua {
+
+extern void lua_version_open(lua_State* L);
+
+}
+
+
 #endif
