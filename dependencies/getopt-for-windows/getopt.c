17f2d96cc50cf353fbcf65dbdb0e43b96f9d9f22
niu2x <niu2x@icloud.com>
2022-06-19 10:11:05 +0800

    bugfix old syntax

--- dependencies/getopt-for-windows/getopt.c
+++ dependencies/getopt-for-windows/getopt.c
@@ -290,7 +290,7 @@ the new indices of the non-options in ARGV after they are moved.  */
 static void exchange(char**);
 #endif
 
-static void exchange(argv) char** argv;
+static void exchange(char** argv)
 {
     int bottom = first_nonopt;
     int middle = last_nonopt;
@@ -365,10 +365,8 @@ static void exchange(argv) char** argv;
 #if defined __STDC__ && __STDC__
 static const char* _getopt_initialize(int, char* const*, const char*);
 #endif
-static const char* _getopt_initialize(argc, argv, optstring)
-int argc;
-char* const* argv;
-const char* optstring;
+static const char* _getopt_initialize(
+    int argc, char* const* argv, const char* optstring)
 {
     /* Start processing options with ARGV-element 1 (since ARGV-element 0
     is the program name); the sequence of previously skipped
@@ -477,13 +475,8 @@ recent call.
 If LONG_ONLY is nonzero, '-' as well as '--' can introduce
 long-named options.  */
 
-int _getopt_internal(argc, argv, optstring, longopts, longind, long_only)
-int argc;
-char* const* argv;
-const char* optstring;
-const struct option* longopts;
-int* longind;
-int long_only;
+int _getopt_internal(char* const* argv, const char* optstring,
+    const struct option* longopts, int* longind, int long_only)
 {
     int print_errors = opterr;
     if (optstring[0] == ':')
@@ -1072,10 +1065,7 @@ int long_only;
     }
 }
 
-int getopt(argc, argv, optstring)
-int argc;
-char* const* argv;
-const char* optstring;
+int getopt(int argc, char* const* argv, const char* optstring)
 {
     return _getopt_internal(
         argc, argv, optstring, (const struct option*)0, (int*)0, 0);
@@ -1100,9 +1090,7 @@ int getopt_long_only(int argc, char* const* argv, const char* options,
 /* Compile with -DTEST to make an executable for use in testing
 the above definition of `getopt'.  */
 
-int main(argc, argv)
-int argc;
-char** argv;
+int main(int argc, char** argv)
 {
     int c;
     int digit_optind = 0;
