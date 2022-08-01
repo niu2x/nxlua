1c31ae57a5511ba8eaecb0df733bb23708ce6c87
niu2x <niu2x@icloud.com>
2022-08-01 10:48:55 +0800

    add lundump/lvm

--- src/lundump.h
+++ src/lundump.h
@@ -11,24 +11,25 @@
 #include "lzio.h"
 
 /* load one chunk; from lundump.c */
- Proto* luaU_undump (lua_State* L, ZIO* Z, Mbuffer* buff, const char* name);
+Proto* luaU_undump(lua_State* L, ZIO* Z, Mbuffer* buff, const char* name);
 
 /* make header; from lundump.c */
- void luaU_header (char* h);
+void luaU_header(char* h);
 
 /* dump one chunk; from ldump.c */
- int luaU_dump (lua_State* L, const Proto* f, lua_Writer w, void* data, int strip);
+int luaU_dump(
+    lua_State* L, const Proto* f, lua_Writer w, void* data, int strip);
 
 /* print one chunk; from print.c */
- void luaU_print (const Proto* f, int full);
+void luaU_print(const Proto* f, int full);
 
 /* for header of binary files -- this is Lua 5.1 */
-#define LUAC_VERSION		0x51
+#define LUAC_VERSION 0x51
 
 /* for header of binary files -- this is the official format */
-#define LUAC_FORMAT		0
+#define LUAC_FORMAT 0
 
 /* size of header of binary files */
-#define LUAC_HEADERSIZE		12
+#define LUAC_HEADERSIZE 12
 
 #endif
