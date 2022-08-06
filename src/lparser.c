ffbb3016743d56810d45661ad5355e8dca9eed97
niu2x <niu2x@icloud.com>
2022-08-06 23:48:29 +0800

    doing lparser

--- src/lparser.c
+++ src/lparser.c
@@ -173,6 +173,97 @@ static void removevars(LexState* ls, int tolevel)
         getlocvar(fs, --fs->nactvar).endpc = fs->pc;
 }
 
+static void enterlevel(LexState* ls)
+{
+    if (++ls->L->nCcalls > LUAI_MAXCCALLS)
+        luaX_lexerror(ls, "chunk has too many syntax levels", 0);
+}
+
+#define leavelevel(ls) ((ls)->L->nCcalls--)
+
+static int block_follow(int token)
+{
+    switch (token) {
+        case TK_ELSE:
+        case TK_ELSEIF:
+        case TK_END:
+        case TK_UNTIL:
+        case TK_EOS:
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+static int statement(LexState* ls)
+{
+    int line = ls->linenumber; /* may be needed for error messages */
+    switch (ls->t.token) {
+        case TK_IF: { /* stat -> ifstat */
+            ifstat(ls, line);
+            return 0;
+        }
+        case TK_WHILE: { /* stat -> whilestat */
+            whilestat(ls, line);
+            return 0;
+        }
+        case TK_DO: { /* stat -> DO block END */
+            luaX_next(ls); /* skip DO */
+            block(ls);
+            check_match(ls, TK_END, TK_DO, line);
+            return 0;
+        }
+        case TK_FOR: { /* stat -> forstat */
+            forstat(ls, line);
+            return 0;
+        }
+        case TK_REPEAT: { /* stat -> repeatstat */
+            repeatstat(ls, line);
+            return 0;
+        }
+        case TK_FUNCTION: {
+            funcstat(ls, line); /* stat -> funcstat */
+            return 0;
+        }
+        case TK_LOCAL: { /* stat -> localstat */
+            luaX_next(ls); /* skip LOCAL */
+            if (testnext(ls, TK_FUNCTION)) /* local function? */
+                localfunc(ls);
+            else
+                localstat(ls);
+            return 0;
+        }
+        case TK_RETURN: { /* stat -> retstat */
+            retstat(ls);
+            return 1; /* must be last statement */
+        }
+        case TK_BREAK: { /* stat -> breakstat */
+            luaX_next(ls); /* skip BREAK */
+            breakstat(ls);
+            return 1; /* must be last statement */
+        }
+        default: {
+            exprstat(ls);
+            return 0; /* to avoid warnings */
+        }
+    }
+}
+
+static void chunk(LexState* ls)
+{
+    /* chunk -> { stat [`;'] } */
+    int islast = 0;
+    enterlevel(ls);
+    while (!islast && !block_follow(ls->t.token)) {
+        islast = statement(ls);
+        testnext(ls, ';');
+        lua_assert(ls->fs->f->maxstacksize >= ls->fs->freereg
+            && ls->fs->freereg >= ls->fs->nactvar);
+        ls->fs->freereg = ls->fs->nactvar; /* free registers */
+    }
+    leavelevel(ls);
+}
+
 static void open_func(LexState* ls, FuncState* fs)
 {
     lua_State* L = ls->L;
